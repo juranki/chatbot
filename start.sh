c1e6f0f08f30384f26c2b95db874dd5735c8d34b
Juhani Rankimies <juhani@juranki.com>
2009-08-21 17:22:37 +0000

    clean up $ERL_LIBS

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ERL_LIBS=".:../rabbitmq-erlang-client:../rabbitmq-erlang-client/deps:../rabbitmq-erlang-client/deps/rabbit_common"
+export ERL_LIBS=".:../rabbitmq-erlang-client:../rabbitmq-erlang-client/deps/rabbit_common"
 
 erl -boot start_sasl -config sys -sname chatbot
 #-s chatbot_app
